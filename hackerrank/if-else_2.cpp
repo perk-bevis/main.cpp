67aa20630fb17d028d9d3ae311163746929d709e
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-07-24 13:30:18 +0700

    if-else_2.cpp

---
+++ hackerrank/if-else_2.cpp
@@ -0,0 +1,20 @@
+Yêu cầu: Cho ba số nguyên a , b và c, hãy tính S = a*(b+c)+b*(a+c).
+
+Input Format
+
+3 số a, b, c trên 1 dòng.
+
+Constraints
+
+-10^8≤a,b,c≤10^8
+
+Output Format
+
+In ra giá trị của biểu thức.
+
+Sample Input 0
+
+1 2 3
+Sample Output 0
+
+13
