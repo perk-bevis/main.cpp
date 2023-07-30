6d26d8692f731c20d3ca70eb8d0c442480e45031
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-07-30 11:48:54 +0700

    Update if-else_3.cpp

--- hackerrank/if-else_3.cpp
+++ hackerrank/if-else_3.cpp
@@ -18,3 +18,16 @@ Sample Input 0
 Sample Output 0
 
 75.20
+
+code:
+#include <iostream>
+#include <iomanip>
+#include <math.h> 
+using namespace std;
+
+int main(){
+	int c; cin >> c;
+	double f =(float)c*9/5+32;
+	cout << fixed << setprecision(2) << f << endl; 
+	return 0; 
+} 
