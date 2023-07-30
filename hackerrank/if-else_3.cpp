616b719593c204941c319e38cbe6ae7674b3c629
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-07-30 09:52:09 +0700

    Create if-else_3.cpp

---
+++ hackerrank/if-else_3.cpp
@@ -0,0 +1,20 @@
+Công thức chuyển đơn vị đo nhiệt độ từ C sang F như sau: F = (C * 9 / 5) + 32. Viết chương trình cho phép nhập vào nhiệt độ đo theo độ C là số nguyên dương không quá 10^6, thực hiện chuyển sang đơn vị đo độ F và in ra màn hình. (Lưu ý luôn lấy 2 chữ số thập phân sau dấu chấm phẩy)
+
+Input Format
+
+Nhiệt độ ở độ C là một số nguyên không âm.
+
+Constraints
+
+0≤n≤10^6
+
+Output Format
+
+Kết quả đổi từ độ C sang độ F
+
+Sample Input 0
+
+24
+Sample Output 0
+
+75.20
