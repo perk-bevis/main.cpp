79cf7655f84b46ef0c9e203fb9ea98e8bef2b053
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-08-02 00:48:02 +0700

    if-else_5.cpp

--- hackerrank/if-else_5.cpp
+++ hackerrank/if-else_5.cpp
@@ -1,1 +1,20 @@
+Có nhiều khoảng cách giữa 2 điểm trong hệ tọa độ ví dụ như khoảng cách Manhattan, Euclid, Minkowski. Nhưng trong chương trình toán phổ thông các bạn sử dụng nhiều nhất là khoảng cách Euclid. Vì vậy bạn hãy tính khoảng cách Euclid giữa 2 điểm trong hệ tọa độ Oxy
 
+Input Format
+
+Tọa độ của 2 điểm (x1, y1) và (x2, y2) là các số nguyên
+
+Constraints
+
+-10^6≤xi,yi≤10^6
+
+Output Format
+
+In ra khoảng cách giữa 2 điểm, lấy độ chính xác 2 số sau dấu phẩy.
+
+Sample Input 0
+
+1 4 4 8
+Sample Output 0
+
+5.00
