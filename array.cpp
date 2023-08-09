a7a2f8f60d7789b3886b31da82f5674169b95284
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-08-09 17:33:55 +0700

    array.cpp

--- array.cpp
+++ array.cpp
@@ -1,1 +1,9 @@
+1. Viết chương trình xuất ra các phần tử là số nguyên tố trong mảng
+2. Cho dãy số nguyên. Viết chương trình tìm giá trị lẻ cuối cùng trong mảng.
+3. Cho dãy số nguyên. Viết chương trình tìm đoạn [a, b] sao cho đoạn này chứa mọi giá trị trong mảng.
+4. Cho dãy số nguyên. Viết chương trình kiểm tra mảng có toàn dương hay không?
+5. Cho dãy số nguyên. Viết chương trình kiểm tra mảng có tăng dần hay không?
+6. Viết chương trình in ra các số siêu nguyên tố có trong mảng.
 
+/* Số siêu nguyên tố: à số mà khi bỏ 1 số tùy ý các chữ số bên phải của nó thì phần còn lại vẫn là 1 số nguyên tố
+vd: 2333 là một số siêu nguyên tố có 4 chữ số vì 233, 23, 2 cũng là các số nguyên tố.
