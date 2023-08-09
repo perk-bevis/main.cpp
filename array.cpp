28331d5975479a0d73d7e21f3d9088d9a78016cd
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-08-09 18:10:33 +0700

    array.cpp

--- array.cpp
+++ array.cpp
@@ -6,7 +6,7 @@
 6. Viết chương trình in ra các số siêu nguyên tố có trong mảng.
 
 /* Số siêu nguyên tố: à số mà khi bỏ 1 số tùy ý các chữ số bên phải của nó thì phần còn lại vẫn là 1 số nguyên tố
-vd: 2333 là một số siêu nguyên tố có 4 chữ số vì 233, 23, 2 cũng là các số nguyên tố.
+vd: 2333 là một số siêu nguyên tố có 4 chữ số vì 233, 23, 2 cũng là các số nguyên tố*/
 code:
 #include <iostream>
 #define MAX 100
