c823e3d6f19e2fbebbe2d8c2fcbc90cbee59de19
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-08-16 03:32:38 +0700

    cursor.cpp

--- cursor.cpp
+++ cursor.cpp
@@ -1,4 +1,18 @@
 miền giá trị: lưu địa chỉ
+#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	a = 5;
+	cout << a << endl;//vùng nhớ
+	cout << &a << endl;// địa chỉ
+	int* p;// ko có vừng nhớ để lưu gias trị
+	p = &a;//trỏ đến nhà của a (địa chỉ) => dùng chung vùng nhớ với a 
+	cout << p << endl;// miền giá trị: lưu địa chỉ nơi trỏ đến
+	cout << *p << endl;//*&a toán tử truy cập vùng nhớ(giá trị nơi trỏ đến)
+	cout << &p << endl;// địa chỉ nơi sinh ra
+
+}
 ========================================= BT vận dụng =============================
 1. Viết chương trình dùng con trỏ quản lí mảng n phần tử. Xuất phần tử lớn nhất và nhỏ nhất trong mảng.
 2. Viết chương trình dùng con trỏ quản lí mảng n phần tử.
