87939b4ad587a0f8065ca229eedd4309d8fa21ee
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-08-16 03:03:32 +0700

    cursor.cpp

--- cursor.cpp
+++ cursor.cpp
@@ -1,1 +1,11 @@
+========================================= BT vận dụng =============================
+1. Viết chương trình dùng con trỏ quản lí mảng n phần tử. Xuất phần tử lớn nhất và nhỏ nhất trong mảng.
+2. Viết chương trình dùng con trỏ quản lí mảng n phần tử.
+Xuất các phần tử là số nguyên tố
+----------- BT nâng cao ---------
+3. Viết chương trình dùng con trỏ quản lí mảng. Xóa phần tử tại vị trí vt nhập từ bàn phím
+----------
 
+4. Viết chương trình dùng con trỏ quản lí mảng. Thêm phần tử có giá trị x tại vị trí vt nhập từ bàn phím
+----------
+5. Viết chương trình dùng con trỏ quản lí mảng. Sắp xếp các phần tử trong mảng tăng dần.
