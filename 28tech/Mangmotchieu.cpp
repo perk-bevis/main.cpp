c86d0cc2aea7d28ae76ae20c6012b3e07f5f3f6f
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-23 11:42:42 +0700

    Mangmotchieu.cpp

--- 28tech/Mangmotchieu.cpp
+++ 28tech/Mangmotchieu.cpp
@@ -1,1 +1,24 @@
-
+#include <iostream>
+#include <math.h> 
+using namespace std;
+bool nt(int n){
+	for(int i=2;i <= sqrt(n);i++){
+		if(n%i==0)
+		    return false;
+  }
+  return n>1;
+}
+int main() {
+	int n;cin>>n;
+	int a[n];
+	for(int i=0; i <n; i++){
+		cin >>a[i];
+	}
+  //c2 
+  for(int &y :a) cin >> y
+  //
+	for(int x : a){
+		cout << x <<" ";
+	}
+	return 0;
+}
