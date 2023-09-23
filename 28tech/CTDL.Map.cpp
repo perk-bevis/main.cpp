dfe29ad88189cd2409a61457cf91f72e490600c3
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-24 04:34:05 +0700

    CTDL.Map.cpp

--- 28tech/CTDL.Map.cpp
+++ 28tech/CTDL.Map.cpp
@@ -28,3 +28,21 @@ output: 1 4
         3 1
         4 1
         5 1
+code:
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    map <int,int> mp;
+    int n; cin >>n;
+    for(int i=0;i <n ;i++){
+        int x; cin >> x;
+        mp[x]++;
+    }
+    for(auto it : mp){
+        cout << it.first << " " << it.second <<endl;
+    }
+    return 0;
+}
