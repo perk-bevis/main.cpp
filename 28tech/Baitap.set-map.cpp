c6429c7c3b0d6f99cb8e3808642deaaf93846ae8
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-23 17:35:12 +0700

    Baitap.set-map.cpp

--- 28tech/Baitap.set-map.cpp
+++ 28tech/Baitap.set-map.cpp
@@ -1,2 +1,15 @@
 🔴 SET()
 nhập vào 1 mảng sau đó đếm số lượng phần tử khác nhau trong mảng
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    set <int> s;
+    for(int i = 0; i<n ;i++){
+        int x ; cin >> x;
+        s.insert(x);
+    }
+    cout << s.size() << endl;
+}
