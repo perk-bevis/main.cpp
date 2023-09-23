8d7cc2a076ab17897c22f01d345443039e65018c
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-23 23:04:36 +0700

    CTDL.set.cpp

--- 28tech/CTDL.set.cpp
+++ 28tech/CTDL.set.cpp
@@ -108,3 +108,25 @@ ex:
 input: 10 3
        1 2 3 1 4 5 1 8 9 10
 output: 3 3 4 5 5 8 9 10
+code::::
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+    int n,k; cin >> n >> k;
+    int a[n];
+    for(int &x : a) cin >> x;
+    multiset <int> ms;
+    for(int i = 0; i<k; i++){
+        ms.insert(a[i]);
+    }
+    for(int i=k;i<n;i++){
+        cout << *ms.rbegin()<< " ";
+        ms.erase(ms.find(a[i-k]));
+        ms.insert(a[i]);
+    }
+    cout << *ms.rbegin() << endl;
+    return 0;
+}
