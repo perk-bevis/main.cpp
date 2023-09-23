92b649e8d1297f8cce5ec964abaf770630da2098
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-23 15:42:56 +0700

    CTDL.set.cpp

--- 28tech/CTDL.set.cpp
+++ 28tech/CTDL.set.cpp
@@ -1,5 +1,5 @@
 size(): trả về kích thước hiện tại của set
-empty() :true nếu set rỗng và ngược lại
-insert(): thêm một phần tử vào trong set
-find():
-count():
+empty(): true nếu set rỗng và ngược lại
+insert(): thêm(chèn) một phần tử vào trong set
+find(): trả về interator trỏ đến phần tử cần tìm kiếm nếu không tìm thấy interator trỏ về end của set
+count(): trả về 
