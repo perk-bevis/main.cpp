cf4f9d983de70c6cf077bacadefc39563f431434
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-23 15:46:29 +0700

    CTDL.set.cpp

--- 28tech/CTDL.set.cpp
+++ 28tech/CTDL.set.cpp
@@ -2,4 +2,4 @@ size(): trả về kích thước hiện tại của set
 empty(): true nếu set rỗng và ngược lại
 insert(): thêm(chèn) một phần tử vào trong set
 find(): trả về interator trỏ đến phần tử cần tìm kiếm nếu không tìm thấy interator trỏ về end của set
-count(): trả về 
+count(): trả về số lần xuất hiện của khoá trong container . nhưng trong set các phần tử chỉ xuất hiện 1 lần nên hàm này có ý nghĩa là sẽ return 1 nếu khoá có trong container và 0 nếu không có 
