441b09754916cbb78168ce88ab8dfc961879a14a
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-24 04:09:07 +0700

    CTDL.Map.cpp

--- 28tech/CTDL.Map.cpp
+++ 28tech/CTDL.Map.cpp
@@ -1,1 +1,21 @@
+🔴 map()
+#include <iostream>
+#include <map>
+using namespace std;
 
+int main(){
+    map <int,int> mp;
+    mp[100]= 200;
+    mp[200]= 300;
+    mp[300]= 400;
+    for(pair <int, int> x:mp){
+        cout << x.first << " " << x.second <<endl;
+    }
+    for(auto it : mp){
+        cout << it.first << " " << it.second <<endl;
+    }
+    for(map <int,int>::iterator it = mp.begin(); it != mp.end();it++){
+        cout << (*it).first << " " << (*it).second <<endl;
+    }
+    return 0;
+}
