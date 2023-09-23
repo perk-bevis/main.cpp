9b7f89ae1f84ac55c499eae6803e7d57226845af
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-23 16:15:15 +0700

    CTDL.set.cpp

--- 28tech/CTDL.set.cpp
+++ 28tech/CTDL.set.cpp
@@ -3,6 +3,7 @@ empty(): true nếu set rỗng và ngược lại
 insert(): thêm(chèn) một phần tử vào trong set
 find(): trả về interator trỏ đến phần tử cần tìm kiếm nếu không tìm thấy interator trỏ về end của set
 count(): trả về số lần xuất hiện của khoá trong container . nhưng trong set các phần tử chỉ xuất hiện 1 lần nên hàm này có ý nghĩa là sẽ return 1 nếu khoá có trong container và 0 nếu không có 
+erase(): có 2 kiểu xoá một là xoá theo interator 2 là xoá theo khoá
 
 🔴 find()
 #include <iostream>
