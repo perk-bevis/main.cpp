c0802eb28aac3d57e08df5f01c26a96a79a02938
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-25 20:55:47 +0700

    Bai3_Lksothuocmang.cpp

--- 28tech/Bai3_Lksothuocmang.cpp
+++ 28tech/Bai3_Lksothuocmang.cpp
@@ -23,3 +23,31 @@ int main(){
     else cout << endl;
     }
 }
+===========
+𝐂𝐨𝐝𝐞 ⭕⭕⭕ 
+SỬ DỤNG MAP:
+#include <iostream>
+#include <map>
+using namespace std;
+int main(){
+    int t; cin>>t;
+    while(t--){
+          int n,m; cin >>n>>m;
+    int a[n], b[m];
+    for(int &x : a) cin >> x;
+    map<int ,bool>mp;
+    for(int i = 0; i<m;i++){
+        int x; cin >>x;
+        mp[x]=true;
+    }
+    bool ok =false;
+    for(int x : a){
+        if(mp.find(x) == mp.end()){
+            cout << x << " ";
+            ok = true;
+        }
+    }
+    if(!ok) cout << " not found\n";
+    else cout << endl;
+    }
+}
