216e1cf06b9364923d6ffa5485f040f6928cb9a1
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-26 02:19:38 +0700

    BAI2_Truyvanphantu.cpp

--- 28tech/BAI2_Truyvanphantu.cpp
+++ 28tech/BAI2_Truyvanphantu.cpp
@@ -1,4 +1,24 @@
 Bài 2:
+⭕⭕⭕cách 1:
+#include <iostream>
+using namespace std;
+int main(){
+    int t; cin>>t;
+    while(t--){
+        int n; cin >> n;
+        int a[n];
+        for(int &x :a) cin >> x;
+        sort(a, a+n);
+        int q; cin >> q;
+        while(q--){
+            int x; cin >> x;
+            if(binary_search(a,a+n,x)){
+                cout << "yes\n";
+            }else cout <<"No\n";
+        }
+    }
+}
+⭕⭕⭕⭕ CÁCH 2:
 #include <iostream>
 #include <set>
 using namespace std;
