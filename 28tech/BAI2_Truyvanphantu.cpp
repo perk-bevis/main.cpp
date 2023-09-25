cc7dd30b3e49d7e2ffd1e9a1710a40baef3cd032
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-26 02:27:18 +0700

    BAI2_Truyvanphantu.cpp

--- 28tech/BAI2_Truyvanphantu.cpp
+++ 28tech/BAI2_Truyvanphantu.cpp
@@ -42,3 +42,25 @@ int main(){
          }
     }
 }
+⭕⭕⭕ cách 3:
+#include <iostream>
+#include <map>
+using namespace std;
+int main(){
+    int t; cin>>t;
+    while(t--){
+        int n; cin >> n;
+        map<int,bool>mp;
+        for(int i= 0; i<n ;i++){
+            int x;cin >>x;
+            mp[x] = true;
+        }
+        int q; cin >> q;
+         while(q--){
+               int x ; cin >> x;
+               if(mp.count(x) == 1) {
+                  cout << "YES\n";
+               }else cout << "NO\n";
+         }
+    }
+}
