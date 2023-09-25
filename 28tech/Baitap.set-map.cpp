95bd060ca7c6dc5a6afa4b8325318c7f51c625e7
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-25 09:57:21 +0700

    Baitap.set-map.cpp

--- 28tech/Baitap.set-map.cpp
+++ 28tech/Baitap.set-map.cpp
@@ -91,3 +91,27 @@ int main()
     }
     return 0;
 }
+Bài 2:
+#include <iostream>
+#include <set>
+using namespace std;
+
+
+int main(){
+    int t ; cin >> t;
+    while(t--){
+        int n;cin>>n;
+        set<int>se;
+        for(int i = 0 ; i < n ; i++){
+              int x ; cin >> x;
+              se.insert(x);
+        }
+         int q; cin >> q;
+         while(q--){
+                  int x ; cin >> x;
+                  if(se.count(x) == 1) {
+                        cout << "YES\n";
+                  } else cout << "NO\n";
+         }
+    }
+}
