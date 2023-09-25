179fce1887fcd4479cdc43673d7bdc446bb43bcf
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-25 10:03:12 +0700

    BAI2_Truyvanphantu.cpp

---
+++ 28tech/BAI2_Truyvanphantu.cpp
@@ -0,0 +1,24 @@
+Bài 2:
+#include <iostream>
+#include <set>
+using namespace std;
+
+
+int main(){
+    int t ; cin >> t;
+    while(t--){
+        int n;cin>>n;
+        set<int>se;
+        for(int i = 0 ; i < n ; i++){
+              int x ; cin >> x;
+              se.insert(x);
+        }
+         int q; cin >> q;
+         while(q--){
+                  int x ; cin >> x;
+                  if(se.count(x) == 1) {
+                        cout << "YES\n";
+                  } else cout << "NO\n";
+         }
+    }
+}
