48d5630e00dda9d620eaadb52e078c11f296cc89
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-26 14:26:14 +0700

    Bai5_kitunhieunhat.cpp

--- 28tech/Bai5_kitunhieunhat.cpp
+++ 28tech/Bai5_kitunhieunhat.cpp
@@ -22,3 +22,28 @@ int main(){
        cout <<(char) (res) << endl;
     }
 }
+🔴⭕🔴⭕ Dùng map:
+#include <iostream>
+#include <map>
+using namespace std;
+using ll = long long;
+int main(){
+    int t; cin>>t;
+    cin.ignore();
+    while(t--){
+       string s;
+       getline(cin,s);
+       map<char,int>mp;
+       for(char x : s){
+           mp[x]++;
+       }
+       char res; int fre = 0;
+       for(auto it : mp){
+           if(it.second > fre){
+               fre = it.second;
+               res = it.first;
+           }
+       }
+       cout << res << endl;
+    }
+}
