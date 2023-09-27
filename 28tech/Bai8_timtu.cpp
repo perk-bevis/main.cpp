99bc30a84a4725861f704c014346329f68cf3bf4
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-28 04:18:48 +0700

    Bai8_timtu.cpp

--- 28tech/Bai8_timtu.cpp
+++ 28tech/Bai8_timtu.cpp
@@ -1,1 +1,27 @@
-
+#include <iostream>
+#include <map>
+#include <sstream>
+using namespace std;
+using ll = long long;
+int main(){
+    int t; cin>>t;
+    cin.ignore();
+    while(t--){
+       string s;
+       getline(cin,s);
+       map<string,int>mp;
+       stringstream ss(s);
+       string word;
+       while(ss >> word){
+           mp[word]++;
+       }
+       string res; int fre =0;
+       for(auto it : mp){
+           if(it.second > fre){
+               fre = it.second;
+               res = it.first;
+           }
+       }
+       cout <<res<< endl;
+    }
+}
