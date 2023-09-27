07bbb0e4edb7f0ca3ded3d347c1e8527e8e7e1e5
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-27 08:13:24 +0700

    Bai6_xauparagram.cpp

--- 28tech/Bai6_xauparagram.cpp
+++ 28tech/Bai6_xauparagram.cpp
@@ -17,3 +17,22 @@ int main(){
        else cout << "no\n";
     }
 }
+⭕🛑⭕🔴cách 2
+#include <iostream>
+#include <set>
+using namespace std;
+using ll = long long;
+int main(){
+    int t; cin>>t;
+    cin.ignore();
+    while(t--){
+       string s;
+       getline(cin,s);
+       map<char, bool> mp;
+       for(char x : s){
+           mp[tolower(x)]= true;
+       }
+       if(mp.size() == 26) cout << "yes\n";
+       else cout << "no\n";
+    }
+}
