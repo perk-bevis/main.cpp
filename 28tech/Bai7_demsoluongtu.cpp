6a9cd725c4cf49262375a3e0b5a44224b86966d2
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-27 08:31:36 +0700

    Bai7_demsoluongtu.cpp

--- 28tech/Bai7_demsoluongtu.cpp
+++ 28tech/Bai7_demsoluongtu.cpp
@@ -17,3 +17,23 @@ int main(){
        cout << se.size()<< endl;
     }
 }
+⭕🔴⭕🔴⭕ cách 2
+#include <iostream>
+#include <set>
+using namespace std;
+using ll = long long;
+int main(){
+    int t; cin>>t;
+    cin.ignore();
+    while(t--){
+       string s;
+       getline(cin,s);
+       map<string,int>mp;
+       stringstream ss(s);
+       string word;
+       while(ss > word){
+           mp[word]=true;
+       }
+       cout << mp.size()<< endl;
+    }
+}
