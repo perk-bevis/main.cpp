d4d1c59621d9e96f76e44f071112aa34db0ac1b7
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-09-30 15:27:28 +0700

    loop.cpp

--- loop.cpp
+++ loop.cpp
@@ -468,3 +468,26 @@ int main() {
 	} 
     return 0;
 }
+🔴cách 2
+#include <iostream>
+using namespace std;
+int main() {
+  int n, i, j, k;
+  cout << "Nhập số hàng của tam giác: ";
+  cin >> n;
+  i = 1;
+  while (i <= n) {
+    j = 1;
+    while (j <= n - i) {
+      cout << " ";
+      j++;
+    }
+    k = 1;
+    while (k <= 2 * i - 1) {
+      cout << "*";
+      k++;
+    }
+    cout << "\n";
+    i++;
+  }
+}
