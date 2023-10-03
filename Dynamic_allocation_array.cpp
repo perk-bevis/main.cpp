f54a72bd1e6484fc40fda98f8f09a828a472c410
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-10-03 17:20:20 +0700

    Dynamic_allocation_array.cpp

--- Dynamic_allocation_array.cpp
+++ Dynamic_allocation_array.cpp
@@ -99,3 +99,79 @@ int main()
     return 0;
 }
 =========thuật toán thêm xoá bằng mảng động ============
+#include <iostream>
+using namespace std;
+
+//ham nhap mang 
+void nhap_mang(int *a,int n){
+    for(int i=0; i < n;i++){
+        cout << "\n nhap phan tu a [" << i << "] =";
+        cin >> a[i];
+    }
+}
+
+// ham xuat mang
+void xuat_mang(int *a,int n){
+    for(int i=0; i < n;i++){
+        cout << a[i] << " ";
+    }
+    
+}
+// ham them phan tu vao vi tri bat ki trong mang
+void them(int *&a, int &n,int vt,int x){
+    //truoc khi them phan tu x vao vt - thi chung ta cap phat vung nho cho mang 
+    //b1 tao mang phu temp chua cac phan tu cua mang a
+    int *temp = new int[n];
+    for(int i=0;i<n;i++){
+        temp[i]= a[i];//bo lan luot cac phan tu cua mang a sang cho mang temp
+        //b2 giai phong vung nho cua a di truoc 
+        delete[] a;
+        //b3 cap phat lai vung nho moi cho a ==> mang a se tang len 1 o nho
+        a = new int[n +1]; //cap phat lai vung nho cho mang a voi so luong phan tu mang tang len 1 o nho
+        //b4 do tat ca cac phan tu cua mang temp ve lai cho mang a
+        for(int i =0;i<n;i++){
+            a[i] = temp[i];
+        }
+        delete[] temp;// giai phong vung nho cho temp khi khong can dung nua
+    }
+    //====ki thuat them====
+    //b1 dời 
+	for (int i = n; i > vt; i--) {
+		a[i] = a[i - 1];
+	}
+    //b2: chèn
+	a[vt] = x;
+	//b3 tăng  
+	n++;// có thay đổi
+}
+int main()
+{
+    int n;
+    do{
+        cout << "\n nhap so luong phan tu mang: ";
+        cin >> n;
+        if(n <= 0){
+            cout << "\n so luong pt khong hop le";
+            system("pause");
+        }
+    }while(n <= 0);
+    int *a = new int[n];
+    cout <<"\n\n\t\t nhap mang\n";
+    nhap_mang(a,n);
+    cout << "\n\n\t\t xuat mang\n" << " ";
+    xuat_mang(a,n);
+    
+    int x;
+    cout << "\n nhap phan tu can them: ";
+    cin>>x;
+    
+    int vt;
+    cout << "\n nhap vi tri can them: ";
+    cin >> vt;
+    
+    them(a,n,vt,x);
+    xuat_mang(a,n);
+    delete[] a;
+    //system("pause");
+    return 0;
+}
