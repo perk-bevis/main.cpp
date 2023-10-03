4d716f88a2d481586088d23953de1c0e97685fcd
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-10-03 12:11:52 +0700

    Dynamic_allocation_array.cpp

--- Dynamic_allocation_array.cpp
+++ Dynamic_allocation_array.cpp
@@ -46,3 +46,54 @@ int main()
     system("pause");
     return 0;
 }
+≠================××××××××××××××××××××
+=================MẢNG ĐỘNG========={{
+#include <iostream>
+using namespace std;
+#define MAX 100
+//ham nhap mang 
+void nhap_mang(int a[],int n){
+    for(int i=0; i < n;i++){
+        cout << "\n nhap phan tu a [" << i << "] =";
+        cin >> a[i];
+    }
+}
+// ham xuat mang
+void xuat_mang(int a[],int n){
+    for(int i=0; i < n;i++){
+        cout << a[i] << " ";
+    }
+}
+//
+void xu_ly(int a[],int n){
+    for(int i =0;i<n;i++){
+        if(a[i] % 2 == 0){
+            cout << a[i] << " ";
+        }
+    }
+}
+int main()
+{
+    int n;
+    do{
+        cout << "\n nhap so luong phan tu mang: ";
+        cin >> n;
+        if(n <= 0){
+            cout << "\n so luong pt khong hop le";
+            system("pause");
+        }
+    }while(n <= 0);
+    // cách 1
+    int *a;
+    a = new int[n];
+    // cách 2
+    int *a = new int[n];
+    cout <<"\n\n\t\t nhap mang";
+    nhap_mang(a,n);
+    cout << "\n\n\t\t xuat mang" << " ";
+    xuat_mang(a,n);
+    cout << "\n\n\t\t  xu ly " << " ";
+    xu_ly(a,n);
+    system("pause");
+    return 0;
+}
