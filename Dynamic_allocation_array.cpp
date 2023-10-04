7ea3d566c629007770a47acde5d9e563ffd23adf
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-10-04 13:34:52 +0700

    Dynamic_allocation_array.cpp

--- Dynamic_allocation_array.cpp
+++ Dynamic_allocation_array.cpp
@@ -50,57 +50,6 @@ int main()
 =================MẢNG ĐỘNG========={{
 #include <iostream>
 using namespace std;
-#define MAX 100
-//ham nhap mang 
-void nhap_mang(int *a,int n){
-    for(int i=0; i < n;i++){
-        cout << "\n nhap phan tu a [" << i << "] =";
-        cin >> a[i];
-    }
-}
-// ham xuat mang
-void xuat_mang(int *a,int n){
-    for(int i=0; i < n;i++){
-        cout << a[i] << " ";
-    }
-}
-//
-void xu_ly(int *a,int n){
-    for(int i =0;i<n;i++){
-        if(a[i] % 2 == 0){
-            cout << a[i] << " ";
-        }
-    }
-}
-int main()
-{
-    int n;
-    do{
-        cout << "\n nhap so luong phan tu mang: ";
-        cin >> n;
-        if(n <= 0){
-            cout << "\n so luong pt khong hop le";
-            system("pause");
-        }
-    }while(n <= 0);
-    // cách 1
-    //int *a;
-    //a = new int[n];
-    // cách 2
-    int *a = new int[n];
-    cout <<"\n\n\t\t nhap mang\n";
-    nhap_mang(a,n);
-    cout << "\n\n\t\t xuat mang\n" << " ";
-    xuat_mang(a,n);
-    cout << "\n\n\t\t  xu ly \n" << " ";
-    xu_ly(a,n);
-    delete[] a;
-    system("pause");
-    return 0;
-}
-=========thuật toán thêm xoá bằng mảng động ============
-#include <iostream>
-using namespace std;
 
 //ham nhap mang 
 void nhap_mang(int *a,int n){
@@ -128,12 +77,12 @@ void them(int *&a, int &n,int vt,int x){
         delete[] a;
         //b3 cap phat lai vung nho moi cho a ==> mang a se tang len 1 o nho
         a = new int[n +1]; //cap phat lai vung nho cho mang a voi so luong phan tu mang tang len 1 o nho
-        //b4 do tat ca cac phan tu cua mang temp ve lai cho mang a
+        }
+     //b4 do tat ca cac phan tu cua mang temp ve lai cho mang a
         for(int i =0;i<n;i++){
             a[i] = temp[i];
         }
         delete[] temp;// giai phong vung nho cho temp khi khong can dung nua
-    }
     //====ki thuat them====
     //b1 dời 
 	for (int i = n; i > vt; i--) {
