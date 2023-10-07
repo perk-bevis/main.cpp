e86afcb6b74d280b4ff420d633c7dc4118a4cdaa
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-10-07 10:01:12 +0700

    cursor.cpp

--- cursor.cpp
+++ cursor.cpp
@@ -29,6 +29,77 @@ int main() {
 }
 ========================================= BT vận dụng =============================
 1. Viết chương trình dùng con trỏ quản lí mảng n phần tử. Xuất phần tử lớn nhất và nhỏ nhất trong mảng.
+🥇🔴viết bằng c
+#include <stdio.h>
+#include <stdlib.h>
+
+// Hàm nhập mảng số nguyên từ bàn phím
+void nhapMang(int *a, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("Nhap a[%d]: ", i);
+        scanf("%d", a + i); // tương đương với &a[i]
+    }
+}
+
+// Hàm xuất mảng số nguyên ra màn hình
+void xuatMang(int *a, int n) {
+    printf("Mang da nhap la: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", *(a + i)); // tương đương với a[i]
+    }
+    printf("\n");
+}
+
+// Hàm tìm phần tử lớn nhất trong mảng
+int timMax(int *a, int n) {
+    int max = *a; // giả sử phần tử đầu tiên là lớn nhất
+    for (int i = 1; i < n; i++) {
+        if (*(a + i) > max) { // nếu phần tử thứ i lớn hơn max
+            max = *(a + i); // cập nhật lại max
+        }
+    }
+    return max;
+}
+
+// Hàm tìm phần tử nhỏ nhất trong mảng
+int timMin(int *a, int n) {
+    int min = *a; // giả sử phần tử đầu tiên là nhỏ nhất
+    for (int i = 1; i < n; i++) {
+        if (*(a + i) < min) { // nếu phần tử thứ i nhỏ hơn min
+            min = *(a + i); // cập nhật lại min
+        }
+    }
+    return min;
+}
+
+int main() {
+    int *a; // khai báo con trỏ a
+    int n; // khai báo số phần tử của mảng
+
+    printf("Nhap so phan tu cua mang: ");
+    scanf("%d", &n);
+
+    a = (int *)malloc(n * sizeof(int)); // cấp phát động bộ nhớ cho mảng a
+
+    if (a == NULL) { // kiểm tra xem có cấp phát được không
+        printf("Khong du bo nho de cap phat!\n");
+        return 0;
+    }
+
+    nhapMang(a, n); // gọi hàm nhập mảng
+    xuatMang(a, n); // gọi hàm xuất mảng
+
+    int max = timMax(a, n); // gọi hàm tìm phần tử lớn nhất
+    int min = timMin(a, n); // gọi hàm tìm phần tử nhỏ nhất
+
+    printf("Phan tu lon nhat trong mang la: %d\n", max);
+    printf("Phan tu nho nhat trong mang la: %d\n", min);
+
+    free(a); // giải phóng bộ nhớ đã cấp phát cho mảng a
+
+    return 0;
+}
+
 2. Viết chương trình dùng con trỏ quản lí mảng n phần tử.
 Xuất các phần tử là số nguyên tố
 ----------- BT nâng cao ---------
