f4ff965dd2174d7bb3039ad5b99a47dd50677c79
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-10-12 23:49:22 +0700

    String_characters.cpp

--- String_characters.cpp
+++ String_characters.cpp
@@ -1,1 +1,15 @@
 
+#include<stdio.h>
+
+int main(){
+  char hoten[30];
+  int namsinh;
+  printf("\n nhap ho ten sinh viên ");
+  fgets(hoten,30,stdin);
+  printf("\n nhap vao nam sinh");
+  scanf("%d", &namsinh);
+  printf("\nho ten sinh viên: %s", hoten);
+  printf("\nnam sinh: %d", namsinh);
+  getch();
+  return 0;
+}
