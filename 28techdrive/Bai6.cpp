f623cdcfdde00fee9e735165960e16ec264c0e09
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-11-01 17:04:41 +0700

    Bai6.cpp

--- 28techdrive/Bai6.cpp
+++ 28techdrive/Bai6.cpp
@@ -1,1 +1,11 @@
 //tính khoảng cách
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main(){
+  int x1,y1,x2,y2; cin >> x1>>y1>>x2>>y2;
+  float khoangcach = sqrt(pow(x2-x1,2)+pow(y2-y1,2));
+  cout << khoangcach << endl;
+}
