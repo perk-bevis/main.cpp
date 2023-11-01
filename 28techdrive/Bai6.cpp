e636af72735a3c9a3646a06d3f6f0b9662fadb0a
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-11-01 17:14:44 +0700

    Bai6.cpp

--- 28techdrive/Bai6.cpp
+++ 28techdrive/Bai6.cpp
@@ -6,6 +6,6 @@ using namespace std;
 
 int main(){
   int x1,y1,x2,y2; cin >> x1>>y1>>x2>>y2;
-  float khoangcach = sqrt(pow(x2-x1,2)+pow(y2-y1,2));
+  double khoangcach = sqrt(pow(x2-x1,2)+pow(y2-y1,2));
   cout << fixed << setprecision(2) << khoangcach << endl;
 }
