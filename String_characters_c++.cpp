ea9b8f539a3d9e92b75feb154448c40701c3c49a
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-11-03 23:43:14 +0700

    String_characters_c++.cpp

--- String_characters_c++.cpp
+++ String_characters_c++.cpp
@@ -18,3 +18,21 @@ int main(){
   system("pause");
   return 0;
 }
+🔴CÁC HÀM STRING TRONG C++
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  string s ="lap trinh";
+  s.erase(s.begin()+3);// xoá 1 kí tự tại vt số 3
+  cout << "\n chuoi sau khi xoa: "<< s;
+  // ===
+  s.insert(s.begin()+ 3, 't');// hàm thêm 1 kí tự 't' tại vị trí số 3
+  cout << "\n chuoi sau khi them: "<< s;
+  cout << "\n do dai cua chuoi: "<< s.length();
+  //*******
+  s.push_back('C'); // hàm thêm 1 kí tự vào cuối chuỗi
+  system("pause");
+  return 0;
+}
