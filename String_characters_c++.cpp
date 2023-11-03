2ddf8748f3aae9346dc8a47a00940168c45aa7a7
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-11-03 23:17:38 +0700

    String_characters_c++.cpp

--- String_characters_c++.cpp
+++ String_characters_c++.cpp
@@ -1,1 +1,20 @@
-//chuỗi trong c++
+🔴chuỗi trong c++
+nhập tên và tuổi sinh viên
+🎈code:
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  string hoten;
+  int tuoi;
+  cout << "\n nhap tuoi sinh vien: " ;
+  cin >> tuoi;
+  cin.ignore();
+  cout<< "\n ho ten sinh viên: ";
+  getline(cin,hoten);
+  cout << "\n ho ten sinh viên: " << hoten;
+  cout << "\n tuoi sinh viên: "<< tuoi;
+  system("pause");
+  return 0;
+}
