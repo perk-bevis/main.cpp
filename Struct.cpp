4921957fceda95bc793efbc6675471a4a922c373
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-11-04 17:39:15 +0700

    Struct.cpp

--- Struct.cpp
+++ Struct.cpp
@@ -203,3 +203,76 @@ int main() {
     cout<< "\n\n\t\t Độ dài cạnh BC: "<<Tinh_Do_Dai_Canh(B,C);
     return 0;
 }
+==⭕⭕________________⭕⭕==
+▶️ struct lồng nhau
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+
+//nhập vào toạ độ 3 đỉnh của 1 tam giác. tính độ dài từng cạnh
+// Khai báo cấu trúc struct toạ độ 
+struct toado {
+    float x; //toạ độ điểm x
+    float y; //toạ độ điểm y
+};
+
+typedef struct toado TOADO;
+
+// Hàm nhập toạ độ 
+void Nhap_Toa_Do(TOADO &td) {
+    cout << "\nNhập vào điểm x: ";
+    cin >> td.x;// nhập toạ độ điểm x
+
+    cout << "Nhập vào điểm y: ";
+    cin >> td.y;// nhập toạ độ điểm y
+}
+
+//hàm xuất toạ độ 
+void Xuat_Toa_Do(TOADO d) {
+    cout << "(" << d.x << "," << d.y << ")";
+}
+
+//hàm tính độ dài cạnh
+float Tinh_Do_Dai_Canh(TOADO A,TOADO B){
+  return sqrt(pow((B.x-A.x),2)+ pow((B.y-A.y),2));
+}
+
+//cấu trúc struct tam giác
+struct tamgiac{
+    TOADO A;
+    TOADO B;
+    TOADO C;
+};
+typedef struct tamgiac TAMGIAC;
+// hàm nhập tam giác
+void Nhap_Toa_Do_Tam_Giac(TAMGIAC &tg){
+    cout << "\n\n\t\t nhập toạ độ điểm A:\n";
+    Nhap_Toa_Do(tg.A);
+    cout << "\n\n\t\t nhập toạ độ điểm B:\n";
+    Nhap_Toa_Do(tg.B);
+    cout << "\n\n\t\t nhập toạ độ điểm C:\n";
+    Nhap_Toa_Do(tg.C);
+}
+
+//hàm xuất toạ độ tam giác
+void Xuat_Toa_Do_Tam_Giac(TAMGIAC tg){
+    // xuất toạ độ 3 điểm A B C
+    cout << "\n\n\t\t toạ độ điểm A:\n";
+    Xuat_Toa_Do(tg.A);
+    cout << "\n\n\t\t toạ độ điểm B:\n";
+    Xuat_Toa_Do(tg.B);
+    cout << "\n\n\t\t toạ độ điểm C:\n";
+    Xuat_Toa_Do(tg.C);
+}
+int main() {
+    TAMGIAC tg;
+    Nhap_Toa_Do_Tam_Giac(tg);
+    Xuat_Toa_Do_Tam_Giac(tg);
+    //độ dài cạnh AB
+    cout<< "\n\n\t\t Độ dài cạnh AB: "<<Tinh_Do_Dai_Canh(tg.A,tg.B);
+    cout<< "\n\n\t\t Độ dài cạnh AC: "<<Tinh_Do_Dai_Canh(tg.A,tg.C);
+    cout<< "\n\n\t\t Độ dài cạnh BC: "<<Tinh_Do_Dai_Canh(tg.B,tg.C);
+    return 0;
+}
