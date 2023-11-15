08a7441694ba9ae4e52818490b676215cc47692c
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-11-16 00:24:37 +0700

    Bai24.cpp

--- 1000codethaykhang/Bai24.cpp
+++ 1000codethaykhang/Bai24.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+  int n; cin >>n;
+  for(int i=1; i<=n;i++){
+    if(n%i==0){
+      cout << i << " ";
+    }
+  }
+  cout << endl;
+  for(int i=1; i<=n;i++){
+    if (n % i == 0 && i % 2 == 1) {
+      cout << i << " ";
+    }
+  }
+}
