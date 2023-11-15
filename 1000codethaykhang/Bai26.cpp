b8c31125279e84d85fc596e3f6318f39701386b8
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-11-16 00:42:27 +0700

    Bai26.cpp

---
+++ 1000codethaykhang/Bai26.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n; cin >>n;
+  for(int i=1; i<=n;i++){
+    if(n%i==0){
+      cout << i << " ";
+    }
+  }
+  cout << endl;
+  int sum = 1;
+  for(int i=1; i<=n;i++){
+    if(n%i==0){
+      if (i % 2 == 1) {
+         sum *= i;
+      }
+    }
+  }
+  cout << sum << endl;
+}
