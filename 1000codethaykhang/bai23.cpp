480629aae273c9e19dfd27643bc3fcb5ff2d9b3c
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-11-16 00:05:17 +0700

    bai23.cpp

--- 1000codethaykhang/bai23.cpp
+++ 1000codethaykhang/bai23.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+  int n; cin >>n;
+  for(int i=1; i<=n;i++){
+    if(n%i==0){
+      cout << i << " ";
+    }
+  }
+  cout << endl;
+  int dem = 0;
+  for(int i=1; i<=n;i++){
+    if(n%i==0){
+      dem++;
+    }
+  }
+  cout << dem << endl;
+}
