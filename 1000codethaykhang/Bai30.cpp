f8b5eff2761eb5b1a2ef692ce72f915c80935ac8
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-12-01 00:42:23 +0700

    Bai30.cpp

--- 1000codethaykhang/Bai30.cpp
+++ 1000codethaykhang/Bai30.cpp
@@ -1,1 +1,33 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int i, n;
+    long S;
+    do
+    {
+    cout <<"\nNhap n(n > 0): " << endl;
+    cin >> n;
+      if(n <= 0)
+       {
+        cout <<"\n N phai > 0. Xin nhap lai !"<< endl;
+       }
+    }while(n <= 0);
+    i = 1;
+    S = 0;
+    while(i < n)
+    {
+        if(n % i == 0)
+        {
+           S = S + i;
+        }
+            i++;
+    }
+    if(S == n)
+        cout << S <<" la so hoan thien" << endl;
+    else
+        cout <<"\nSo nhap vao khong la so hoan thien"<< endl;
+    getch();
+    return 0;
+}
