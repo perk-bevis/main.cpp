6565f409cf42a8ab545168f2d05ee28ecf8f0d59
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-12-06 23:09:55 +0700

    Bai32.cpp

--- 1000codethaykhang/Bai32.cpp
+++ 1000codethaykhang/Bai32.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+  int n;
+  cout << "\nNhap n = ";
+  cin >> n;
+  int i = 0;
+  while(i*i <= n){
+    if(i*i == n){
+      cout << n << " la so chinh phuong!\n";
+      return 0;
+    }
+    ++i;
+  }
+  cout << n << " khong phai so chinh phuong!\n";
+} 
