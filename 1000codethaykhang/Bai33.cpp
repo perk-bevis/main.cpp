675d32fdcf7e6938f57656e286056d23b6e30d7b
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-12-09 12:14:09 +0700

    Bai33.cpp

--- 1000codethaykhang/Bai33.cpp
+++ 1000codethaykhang/Bai33.cpp
@@ -1,2 +1,22 @@
 Bài 33: Tính S(n) = CanBac2(2+CanBac2(2+….+CanBac2(2 + CanBac2(2)))) có n dấu căn
 _________________
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "nhap n(n>1): ";
+    cin >> n;
+    float S;
+    S = sqrt((float)2);
+    for(int i = 2; i <= n; i++)
+    {
+        S = sqrt(2 + S);
+    }
+    cout << S << endl;
+
+    getch();
+    return 0;
+}
