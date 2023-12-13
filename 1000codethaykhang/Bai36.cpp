5fb459f27b6e9634aa8199ab13b056ac0355c25f
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-12-13 11:40:18 +0700

    Bai36.cpp

--- 1000codethaykhang/Bai36.cpp
+++ 1000codethaykhang/Bai36.cpp
@@ -1,1 +1,24 @@
 Bài 36: Tính S(n) = CanBac2(n! + CanBac2((n-1)! +CanBac2((n – 2)! + … + CanBac2(2!) + CanBac2(1!)))) có n dấu căn
+__________
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+  int n;
+  double S, M;
+  do {
+    cout << "\nNhap n (n >= 1): ";
+    cin >> n;
+    if (n < 1) {
+      cout << "\nn phai >= 1. Xin nhap lai !";
+    }
+  } while (n < 1);
+  M = 1;
+  S = sqrt(0.0);
+  for (int i = 1; i <= n; i++) {
+    M = M * i;
+    S = sqrt(M + S);
+  }
+  cout << "\nTong la " << S;
+  return 0;
+}
