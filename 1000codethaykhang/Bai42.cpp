269d5e3ad9bbb20151094edd6d8b97e6b5ea0a3c
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-12-18 21:55:55 +0700

    Bai42.cpp

--- 1000codethaykhang/Bai42.cpp
+++ 1000codethaykhang/Bai42.cpp
@@ -1,1 +1,14 @@
 Bài 42: Cho n là số nguyên dương. Hãy tìm giá trị nguyên dương k lớn nhất sao cho S(k)  < n. Trong đó chuỗi k được định nghĩa như sau: S(k) = 1 + 2 + 3 + … + k
+// dùng while
+#include <iostream>
+using namespace std;
+
+int main(){
+  long long int n ,i=0,s=0;
+  cin >> n;
+  while(s+i<n){
+    i++;
+    s+=i;
+  }
+  cout << i<< endl;
+}
