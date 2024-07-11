704a12740ea019f09fb39774be80b56ba6103f80
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-07-11 16:21:45 +0700

    Homework.cpp

--- Homework.cpp
+++ Homework.cpp
@@ -9,19 +9,19 @@ int main(){
  printf("\nVui long nhap lai So Dien\nSo dien khong duoc nho hon 0\n");
  }else if(soDien<=100){
   tienDien=soDien*bac1;
-  printf("\nban da dung dien trong khoang 0 - 100 kW so tien ban phai tra la: %.2f",tienDien);
+  printf("\nban da dung dien trong khoang 0 - 100 kW so tien ban phai tra la:",tienDien);
  }else if(soDien<=150){
   tienDien=100*bac1+((soDien-100)*bac2);
-  printf("\nban da dung dien trong khoang 101 - 150 kW so tien ban phai tra la: %.2f",tienDien);
+  printf("\nban da dung dien trong khoang 101 - 150 kW so tien ban phai tra la:",tienDien);
  }else if(soDien<=200){
   tienDien=100*bac1+(50*bac2)+((soDien-150)*bac3);
-  printf("\nban da dung dien trong khoang 151 - 200 kWh so tien ban phai tra la: %.2f",tienDien);
+  printf("\nban da dung dien trong khoang 151 - 200 kWh so tien ban phai tra la:",tienDien);
  }else if(soDien<=300){
   tienDien=100*bac1+(50*bac2)+(50*bac3)+((soDien-200)*bac4);
-  printf("\nban da dung dien trong khoang 201 - 300 kWh so tien ban phai tra la: %.2f",tienDien);
+  printf("\nban da dung dien trong khoang 201 - 300 kWh so tien ban phai tra la:",tienDien);
  }else if(soDien>300){
   tienDien=100*bac1+(50*bac2)+(50*bac3)+(100*bac4)+((soDien-300)*bac5);
-  printf("\nban da dung dien tren 400 kWh so tien ban phai tra la: %.2f",tienDien);
+  printf("\nban da dung dien tren 400 kWh so tien ban phai tra la:",tienDien);
  }
  return 0;
 }
