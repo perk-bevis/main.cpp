dc861e3763ce94c7b8573c7eaefa1f39fc984c7e
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-09-09 10:31:07 +0700

    Bai41.cpp

--- 1000codethaykhang/Bai41.cpp
+++ 1000codethaykhang/Bai41.cpp
@@ -1,1 +1,24 @@
 Bài 41: Tính S(n) = 1 / (1 + 1 / ( 1 + 1 / (…. 1 + 1 / 1 + 1))) có n dấu phân số
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i, n;
+    float S;
+    i = 0;
+    S = 1;
+
+    cout << "\nNhap n: ";
+    cin >> n;
+
+    while(i <= n)
+    {
+        S = 1 + 1.0/S;
+        i++;
+    }
+    
+    cout << "\nTong la " << S;
+    return 0;
+}
