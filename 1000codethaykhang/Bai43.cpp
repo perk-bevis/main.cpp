3b04fe1b24ff32051a3380cf4bf0ad3d12d6d7a9
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-10-10 10:56:01 +0700

    Bai43.cpp

--- 1000codethaykhang/Bai43.cpp
+++ 1000codethaykhang/Bai43.cpp
@@ -24,3 +24,25 @@ int main() {
 
     return 0;
 }
+cách 2:
+#include <iostream>
+using namespace std;
+
+int main() {
+    long n;
+    int number = 0;
+    do {
+        cout << "\nNhap n: ";
+        cin >> n;
+    } while (n < 0 && cout << "\nLoi: n >= 0 !");
+    for (long count = n; count > 0; ) {
+        count /= 10;
+        number++;
+    }
+    if (n == 0) {
+        number = 1; 
+    }
+    cout << "\nSo chu so cua " << n << " la " << number;
+
+    return 0;
+}
