d59e086e75709f0b7b08649c35358333686a3fdc
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-10-15 09:46:08 +0700

    Bai51.cpp

--- 1000codethaykhang/Bai51.cpp
+++ 1000codethaykhang/Bai51.cpp
@@ -1,1 +1,22 @@
 Bài 51: Tìm chữ số lớn nhất củɑ số nɡuyên dươnɡ n
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,i;
+    int max = 0;
+    do {
+        cout << "\nNhap n: ";
+        cin >> n;
+    } while (n < 0 && cout << "\nLoi: n >= 0 !");
+    int temp = n;
+    for ( ;n > 0; n/=10 ) {
+        i = n % 10;
+        if(i > max){
+          max = i;
+        }
+    }
+    cout << "\nSo lớn nhất của số nguyên n la " << temp << " la " << max;
+
+    return 0;
+}
