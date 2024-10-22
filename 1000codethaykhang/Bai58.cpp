1e8fd71efbf0538abe0eae52238439dde0a2e62c
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-10-22 18:19:35 +0700

    Bai58.cpp

--- 1000codethaykhang/Bai58.cpp
+++ 1000codethaykhang/Bai58.cpp
@@ -1,1 +1,15 @@
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string n;
+    cin >> n;
+    int size_of_N = n.size();
+    if(static_cast<char> (n[0]) == '-'){
+        --size_of_N;
+    }
+    cout<< size_of_N;
+}
+
