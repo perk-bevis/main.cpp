51d2c8df70b00494d02ee4cb096441446808060d
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-10-22 18:10:14 +0700

    Bai43.cpp

--- 1000codethaykhang/Bai43.cpp
+++ 1000codethaykhang/Bai43.cpp
@@ -1,4 +1,31 @@
+Bài 43: Hãy đếm số lượnɡ chữ số củɑ số nɡuyên dươnɡ n
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+int main() {
+    long themang, n;
+    int sochuso;
+
+    do {
+        cout << "\nNhap n: ";
+        cin >> n;
+    } while (n < 0 && cout << "\nLoi: n >= 0 !");
+    
+    sochuso = 0;
+    themang = n;
+
+    if (n == 0)
+        sochuso = 1;
+    while (themang != 0) {
+        sochuso = sochuso + 1;
+        themang = themang / 10;
+    }
+    cout << "\nSo chu so cua " << n << " la " << sochuso;
+
+    return 0;
+}
+cách 2:
 #include <iostream>
 using namespace std;
 
