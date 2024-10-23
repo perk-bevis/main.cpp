14a758d4836ca94a0ccbcee7850f7b9a8740b557
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-10-23 09:10:45 +0700

    Bai60.cpp

--- 1000codethaykhang/Bai60.cpp
+++ 1000codethaykhang/Bai60.cpp
@@ -1,1 +1,41 @@
 Bài 60: Hãy kiểm tra các chữ số của số nguyên dương n có tăng dần từ trái sang phải hay không
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Nhap n: ";
+    cin >> n;
+
+    int pt1, pt2;
+    bool tangdan = true;
+    int temp = n;         
+  
+    int divisor = 1;
+    while (temp / divisor >= 10) {
+        divisor *= 10; 
+    }
+
+    pt2 = temp / divisor;  
+    temp %= divisor;      
+    divisor /= 10;         
+
+    while (divisor > 0) {
+        pt1 = temp / divisor; 
+        if (pt2 > pt1) {      
+            tangdan = false;
+            break;
+        }
+        pt2 = pt1;  
+        temp %= divisor;  
+        divisor /= 10;    
+    }
+
+    if (tangdan) {
+        cout << n << " la so tang dan tu trai sang phai" << endl;
+    } else {
+        cout << n << " khong phai la so tang dan tu trai sang phai" << endl;
+    }
+
+    return 0;
+}
