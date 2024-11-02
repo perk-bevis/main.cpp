edd5f6e23b03426dfcdf32108ce5bdd09bfe4f4c
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-11-02 17:25:51 +0700

    Function.cpp

--- hackerrank/Function.cpp
+++ hackerrank/Function.cpp
@@ -1,3 +1,20 @@
+Input will contain four integers -a,b,c,d  , one per line.
+
+Output Format
+
+Return the greatest of the four integers.
+PS: I/O will be automatically handled.
+
+Sample Input
+
+3
+4
+6
+5
+Sample Output
+
+6
+code:
 #include <iostream>
 using namespace std;
 
