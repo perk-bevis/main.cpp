7029ea860a7aeec98e814ffebe5963e8176e5dac
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-11-02 16:34:24 +0700

    if-else_9.cpp

--- hackerrank/if-else_9.cpp
+++ hackerrank/if-else_9.cpp
@@ -27,3 +27,33 @@ nine
 even
 odd
 code:
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    for (int n = a; n <= b; ++n) {
+        if (n >= 1 && n <= 9) {
+            switch (n) {
+                case 1: cout << "one" << endl; break;
+                case 2: cout << "two" << endl; break;
+                case 3: cout << "three" << endl; break;
+                case 4: cout << "four" << endl; break;
+                case 5: cout << "five" << endl; break;
+                case 6: cout << "six" << endl; break;
+                case 7: cout << "seven" << endl; break;
+                case 8: cout << "eight" << endl; break;
+                case 9: cout << "nine" << endl; break;
+            }
+        } else {
+            if (n % 2 == 0) {
+                cout << "even" << endl;
+            } else {
+                cout << "odd" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
