9b71b55a45d22080e58fac01940582401f4eeda7
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-11-02 15:33:25 +0700

    if-else_9.cpp

--- hackerrank/if-else_9.cpp
+++ hackerrank/if-else_9.cpp
@@ -1,27 +1,28 @@
 Định dạng đầu vào
 
-Bạn sẽ được cho hai số nguyên dương,  và  (  ), mỗi số nằm trên một dòng.
+Bạn sẽ được cho hai số nguyên dương, a và b (với a nhỏ hơn hoặc bằng b), mỗi số nằm trên một dòng.
 
 Định dạng đầu ra
 
-Với mỗi số nguyên  trong đoạn bao gồm cả  và :
+Với mỗi số nguyên n trong đoạn từ a đến b (bao gồm cả a và b):
 
-Nếu , in ra dạng chữ của nó bằng tiếng Anh, viết thường. Ví dụ, "one" cho 1, "two" cho 2, v.v.
+Nếu n nằm trong khoảng từ 1 đến 9, in ra tên tiếng Anh của nó (bằng chữ thường). Ví dụ: "one" cho 1, "two" cho 2, v.v.
 
-Nếu  và là số chẵn, in ra "even".
+Nếu n lớn hơn 9 và là số chẵn, in ra "even".
 
-Nếu  và là số lẻ, in ra "odd".
+Nếu n lớn hơn 9 và là số lẻ, in ra "odd".
 
 
-Lưu ý: 
+Lưu ý: Đoạn từ a đến b là tập hợp các số nguyên x mà a nhỏ hơn hoặc bằng x và x nhỏ hơn hoặc bằng b, tức là các số từ a, a + 1, …, đến b.
 
 Ví dụ đầu vào
+
 8
 11
+
 Ví dụ đầu ra
 
 eight
 nine
 even
 odd
-
