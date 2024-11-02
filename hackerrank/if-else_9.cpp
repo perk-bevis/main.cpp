00a85e368f9f6faeda75adc9b0634ad30b43a072
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-11-02 15:29:32 +0700

    if-else_9.cpp

--- hackerrank/if-else_9.cpp
+++ hackerrank/if-else_9.cpp
@@ -1,1 +1,27 @@
+Định dạng đầu vào
+
+Bạn sẽ được cho hai số nguyên dương,  và  (  ), mỗi số nằm trên một dòng.
+
+Định dạng đầu ra
+
+Với mỗi số nguyên  trong đoạn bao gồm cả  và :
+
+Nếu , in ra dạng chữ của nó bằng tiếng Anh, viết thường. Ví dụ, "one" cho 1, "two" cho 2, v.v.
+
+Nếu  và là số chẵn, in ra "even".
+
+Nếu  và là số lẻ, in ra "odd".
+
+
+Lưu ý: 
+
+Ví dụ đầu vào
+8
+11
+Ví dụ đầu ra
+
+eight
+nine
+even
+odd
 
