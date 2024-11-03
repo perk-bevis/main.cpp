9ab44c0bf93f9b716cf32d22777d97d006ba7472
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-11-03 10:34:51 +0700

    Arrays_Introduction.cpp

--- hackerrank/Arrays_Introduction.cpp
+++ hackerrank/Arrays_Introduction.cpp
@@ -1,1 +1,45 @@
+The first line of the input contains ,where  is the number of integers.The next line contains  space-separated integers.
 
+Constraints
+
+1 <= n <= 1000
+1 <= a[i] <= 10000
+
+
+, where  is the  integer in the array.
+
+Output Format
+
+Print the  integers of the array in the reverse order, space-separated on a single line.
+
+Sample Input
+
+4
+1 4 3 2
+Sample Output
+
+2 3 4 1
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Nhap n: ";
+    cin >> n;
+    long long a[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for(int i = 0; i < n / 2; i++) {
+        long long temp = a[i];
+        a[i] = a[n - i - 1];
+        a[n - i - 1] = temp;
+    }
+    for(int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    
+    return 0;
+}
