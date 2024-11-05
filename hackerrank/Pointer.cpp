d03f9037a8c028fb5eac2acf6e14aa773b7d2dbf
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-11-05 10:12:47 +0700

    Pointer.cpp

--- hackerrank/Pointer.cpp
+++ hackerrank/Pointer.cpp
@@ -1,3 +1,17 @@
+Yêu cầu: Viết hàm void có tên update để cập nhật hai biến a và b. Hàm không trả về giá trị mà sẽ thay đổi giá trị của a và b trong bộ nhớ để:
+a chứa tổng của hai số ban đầu (a' = a + b)
+b chứa độ chênh lệch tuyệt đối giữa hai số ban đầu (b' = |a - b|)
+Định dạng đầu vào: Chứa hai số nguyên a và b, cách nhau bởi dòng mới.
+Ví dụ đầu vào:
+
+4
+5
+
+Ví dụ đầu ra:
+
+9
+1
+code🔴
 #include <iostream>
 #include <cmath>
 using namespace std;
