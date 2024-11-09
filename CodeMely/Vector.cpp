62c155a2d5cca1fb8a38877d0aa69a35fc15f114
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-11-09 17:29:04 +0700

    Vector.cpp

--- CodeMely/Vector.cpp
+++ CodeMely/Vector.cpp
@@ -44,3 +44,39 @@ int main() {
     return 0;
 }
 
+🔴
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n,m; 
+    cin >> n >>m;
+    int a[n], b[m];
+    for(int i = 0; i < n; i++){
+       cin >> a[i];
+    }
+    for(int i = 0; i < m; i++){
+       cin >> b[i];
+    }
+
+    int i = 0, j = 0;
+    while(i < n && j < m){
+    // so sanh a[i] b[j]
+        if(a[i] <= b[j]){
+           cout << a[i] << ' '; ++i;
+    }
+        else{
+           cout << b[j] << ' '; ++j;
+        }
+    }
+
+    while(i < n){
+        cout << a[i] << ' '; ++i;
+    }
+    while(j < m){
+    cout << b[j] << ' '; ++j;
+    }
+
+    return 0;
+}
