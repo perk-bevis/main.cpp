7366ed5c879c60007f7e282692eea7e1b22a22b6
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-11-09 17:42:45 +0700

    Vector.cpp

--- CodeMely/Vector.cpp
+++ CodeMely/Vector.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+⁸#include <iostream>
 #include <vector>
 using namespace std;
 
@@ -80,3 +80,50 @@ int main() {
 
     return 0;
 }
+
+🔴
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n,m; 
+    cin >> n >>m;
+    int a[n], b[m];
+    for(int i = 0; i < n; i++){
+       cin >> a[i];
+    }
+    for(int i = 0; i < m; i++){
+       cin >> b[i];
+    }
+
+    int i = 0, j = 0;
+    vector<int> hop,giao;
+    while(i < n && j < m){
+       if(a[i] == b[j]){
+         giao.push_back(a[i]);
+         hop.push_back(a[i]);
+         ++i; ++j;
+       } else if(a[i] < b[j]){
+         hop.push_back(a[i]);
+         ++i;
+       }else{
+         hop.push_back(b[j]);
+         ++j;
+       }
+    }
+
+    while(i < n){
+        hop.push_back(a[i]);
+         ++i;
+    }
+    while(j < m){
+        hop.push_back(b[j]);
+         ++j;
+    }
+    for(int x : giao) cout << x << " ";
+    cout << endl;
+    for(int x : hop) cout << x << " ";
+    return 0;
+}
+
