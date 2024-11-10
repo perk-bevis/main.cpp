bd79572d41f7928d462f1044ed8badd8c6689bfc
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-11-10 22:21:03 +0700

    Basic.cpp

--- 28tech/Basic.cpp
+++ 28tech/Basic.cpp
@@ -29,3 +29,30 @@ int main() {
 
     return 0; 
 }
+===== CÁCH 2 =====
+#include <iostream>
+#include <vector>
+#include <string.h>
+using namespace std;
+
+int main() { 
+  int n; cin >> n; 
+  vector<char> A(n); 
+  for(int i = 0; i < n; i++) { 
+    cin >> A[i]; 
+  } 
+  string tmp = "28tech";
+  int len = tmp.length(); 
+  int j = 0; 
+  for(int i = 0; i < n; i += 2) { 
+    if(A[i] == tmp[j]) { 
+      j++;
+    } 
+    if(j == len) { 
+      break; 
+    } 
+  }
+  if(j == len) cout << "28TECH" << endl; 
+  else cout << "HCET82" << endl; 
+  return 0; 
+}
