0e737282279b1c95cf1b581e95e96051a3b65ee0
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-11-10 21:45:45 +0700

    basic.cpp

--- hnoj/basic.cpp
+++ hnoj/basic.cpp
@@ -16,3 +16,30 @@ int main() {
     }
     return x / 6;
 }
+🔴vector
+#include <iostream>
+#include <vector>
+#include <string.h>
+using namespace std;
+
+int main() { 
+  int n; cin >> n; 
+  vector<char> A(n); 
+  for(int i = 0; i < n; i++) { 
+    cin >> A[i]; 
+  } 
+  string tmp = "28tech";
+  int len = tmp.length(); 
+  int j = 0; 
+  for(int i = 0; i < n; i += 2) { 
+    if(A[i] == tmp[j]) { 
+      j++;
+    } 
+    if(j == len) { 
+      break; 
+    } 
+  }
+  if(j == len) cout << "28TECH" << endl; 
+  else cout << "HCET82" << endl; 
+  return 0; 
+}
