9e990824de364ec209dddca5e30c385e1b035d16
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-11-11 22:52:30 +0700

    Qhd.cpp

---
+++ Algorithm/Qhd.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#define max_dong 1005
+#define max_cot 1005
+using namespace std;
+
+void sang(int a[max_dong][max_cot], int n) {
+    a[1][0] = 1;
+    a[1][1] = 1;
+    for (int i = 2; i <= n; i++) {
+        for (int j = 0; j <= i; j++) {
+            if (j == 0 || j == i) {
+                a[i][j] = 1;
+            } else {
+                a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+            }
+        }
+    }
+}
+
+int main() {
+    int n, k;
+    cin >> k >> n;
+    int a[max_dong][max_cot];
+    sang(a, n);
+    cout << a[n][k] << endl;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
