dad1cd49f1eabcd030f00dda71323dc2d1e9405b
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-11-13 18:29:54 +0700

    Baitoancaitui.cpp

--- Algorithm/dynamic_programming/Baitoancaitui.cpp
+++ Algorithm/dynamic_programming/Baitoancaitui.cpp
@@ -34,3 +34,50 @@ int main() {
     cout << ans;
     return 0;
 }
+===============
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#define MAX 5000
+using namespace std;
+
+struct Item {
+    int w, v;
+};
+
+Item dp[MAX];
+Item a[MAX];
+
+int main() {
+    int n, b;
+    cin >> n >> b;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].w;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].v;
+    }
+
+    memset(dp, 0, sizeof(dp));
+
+    dp[0].w = 1; // Khởi tạo trạng thái ban đầu
+    for (int i = 0; i < n; i++) {
+        for (int j = b; j >= a[i].w; j--) {
+            if (dp[j - a[i].w].w) {
+                dp[j].w = 1;
+                dp[j].v = max(dp[j - a[i].w].v + a[i].v, dp[j].v);
+            }
+        }
+    }
+
+    int ans = 0;
+    for (int i = b; i >= 0; i--) {
+        if (dp[i].w)
+            ans = max(ans, dp[i].v);
+    }
+
+    cout << ans;
+    return 0;
+}
