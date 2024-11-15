6e362f253357b1184c2f32432839a9f60b3ab9c0
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-11-15 08:53:55 +0700

    jfjf.cpp

---
+++ 28tech/Jjfjf.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    for (int test = 1; test <= t; test++) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        int res = 0, cnt = 0, dem = 1;
+        vector<int> b;
+
+        for (int i = 1; i < n; i++) {
+            if (a[i] > a[i - 1]) {
+                dem++;
+            } else {
+                dem = 1;
+            }
+
+            if (dem > res) {
+                res = dem;
+                b.clear(); 
+                b.push_back(i - res + 1); 
+                cnt = 1;
+            } else if (dem == res) {
+                b.push_back(i - res + 1); 
+                cnt++;
+            }
+        }
+
+        
+        cout << "Test #" << test << ":" << endl;
+        cout << res << endl;
+
+      
+        for (int i = 0; i < cnt; i++) {
+            for (int j = 0; j < res; j++) {
+                cout << a[b[i] + j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
