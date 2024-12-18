321ee8b3846261a0affd72fbacabef8e81fd6483
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-12-18 13:23:23 +0700

    Update Cặp số chia hết cho 28.cpp

--- 28tech/Cặp số chia hết cho 28.cpp
+++ 28tech/Cặp số chia hết cho 28.cpp
@@ -14,18 +14,14 @@ int main() {
 
     long long result = 0;
 
-    // Cặp (0, 0)
     result += (long long)count[0] * (count[0] - 1) / 2;
 
-    // Cặp (x, 28-x) với x từ 1 đến 13
     for (int i = 1; i <= 13; i++) {
         result += (long long)count[i] * count[28 - i];
     }
-
-    // Cặp (14, 14)
+    
     result += (long long)count[14] * (count[14] - 1) / 2;
 
-    // In kết quả
     cout << result << endl;
 
     return 0;
