e4b08e146db41de5503054a2c16e0116422b7c9f
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-12-18 13:24:54 +0700

    Update Tổng dãy số.cpp

--- 28tech/Tổng dãy số.cpp
+++ 28tech/Tổng dãy số.cpp
@@ -33,3 +33,29 @@ int main() {
 
     return 0;
 }
+// cách 2🔴
+#include <iostream>
+#define max 1000
+using namespace std;
+
+int main() {
+    int n;
+    cout << "n = ";
+    int a[max];
+    cin >> n;
+    for(int i=0;i<n;i++){
+      cin>> a[i];
+    }
+    
+    for (int i = 2; i <= 4; i++) { 
+      for (int j = 0; j <= n - i; j++) { 
+        int sum = 0; 
+            for (int k = j; k < j + i; k++) { 
+                sum += a[k];
+        } 
+        cout << sum << " "; 
+        } 
+        cout << endl; 
+    }
+    return 0;
+}
