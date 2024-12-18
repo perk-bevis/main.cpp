1b57427499d57b849fc46bee092abf264ff8c134
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-12-18 13:15:42 +0700

    Update Tổng dãy số.cpp

--- 28tech/Tổng dãy số.cpp
+++ 28tech/Tổng dãy số.cpp
@@ -1,26 +1,39 @@
+
 #include <iostream>
-#define max 1000
+#include <vector>
 using namespace std;
 
 int main() {
     int n;
-    cout << "n = ";
-    int a[max];
     cin >> n;
-    for(int i=0;i<n;i++){
-      cin>> a[i];
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
     }
-    
-    for (int i = 2; i <= 4; i++) { 
-      for (int j = 0; j <= n - i; j++) { 
-        int sum = 0; 
-            for (int k = j; k < j + i; k++) { 
-                sum += a[k];
-        } 
-        cout << sum << " "; 
-        } 
-        cout << endl; 
+
+    // Tính tổng 2 số liên tiếp
+    for (int i = 0; i <= n - 2; i++) {
+        cout << a[i] + a[i + 1] << " ";
     }
+    cout << endl;
+
+    // Tính tổng 3 số liên tiếp
+    int sum = a[0] + a[1] + a[2]; // Tổng của 3 phần tử đầu tiên
+    cout << sum << " ";
+    for (int i = 3; i < n; i++) {
+        sum = sum - a[i - 3] + a[i]; // Loại phần tử cũ, thêm phần tử mới
+        cout << sum << " ";
+    }
+    cout << endl;
+
+    // Tính tổng 4 số liên tiếp
+    sum = a[0] + a[1] + a[2] + a[3]; // Tổng của 4 phần tử đầu tiên
+    cout << sum << " ";
+    for (int i = 4; i < n; i++) {
+        sum = sum - a[i - 4] + a[i]; // Loại phần tử cũ, thêm phần tử mới
+        cout << sum << " ";
+    }
+    cout << endl;
+
     return 0;
 }
-
