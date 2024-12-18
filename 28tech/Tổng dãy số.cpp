b1ce4b00bdf49f3279770cce850d591561454319
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-12-18 13:18:32 +0700

    Update Tổng dãy số.cpp

--- 28tech/Tổng dãy số.cpp
+++ 28tech/Tổng dãy số.cpp
@@ -1,12 +1,12 @@
-
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
-    vector<int> a(n);
+    int a[1000];
+
+    // Nhập mảng
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
