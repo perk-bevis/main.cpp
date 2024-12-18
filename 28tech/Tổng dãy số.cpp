11b18331ba838d24243679eba2c584d87bef7cee
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2024-12-18 13:20:59 +0700

    Update Tổng dãy số.cpp

--- 28tech/Tổng dãy số.cpp
+++ 28tech/Tổng dãy số.cpp
@@ -6,31 +6,27 @@ int main() {
     cin >> n;
     int a[1000];
 
-    // Nhập mảng
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
 
-    // Tính tổng 2 số liên tiếp
     for (int i = 0; i <= n - 2; i++) {
         cout << a[i] + a[i + 1] << " ";
     }
     cout << endl;
 
-    // Tính tổng 3 số liên tiếp
-    int sum = a[0] + a[1] + a[2]; // Tổng của 3 phần tử đầu tiên
+    int sum = a[0] + a[1] + a[2]; 
     cout << sum << " ";
     for (int i = 3; i < n; i++) {
-        sum = sum - a[i - 3] + a[i]; // Loại phần tử cũ, thêm phần tử mới
+        sum = sum - a[i - 3] + a[i];
         cout << sum << " ";
     }
     cout << endl;
-
-    // Tính tổng 4 số liên tiếp
-    sum = a[0] + a[1] + a[2] + a[3]; // Tổng của 4 phần tử đầu tiên
+    
+    sum = a[0] + a[1] + a[2] + a[3];
     cout << sum << " ";
     for (int i = 4; i < n; i++) {
-        sum = sum - a[i - 4] + a[i]; // Loại phần tử cũ, thêm phần tử mới
+        sum = sum - a[i - 4] + a[i];
         cout << sum << " ";
     }
     cout << endl;
