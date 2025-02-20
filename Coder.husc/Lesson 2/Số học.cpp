83bebef7bcc9dd794e3b0d7cf5e1796dfc024118
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2025-02-20 22:24:13 +0700

    Update Số học.cpp

--- Coder.husc/Lesson 2/Số học.cpp
+++ Coder.husc/Lesson 2/Số học.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    cout << a << " " << "+" << " "<< b <<" " << "=" <<" "<< a+b << endl;
+    cout << a << " " << "-" << " "<< b <<" " << "=" <<" "<< a-b << endl;
+    cout << a << " " << "*" << " "<< b <<" " << "=" <<" "<< a*b << endl;
+    cout << a << " " << "/" << " "<< b <<" " << "=" <<" "<< a/b << endl;
+    cout << a << " " << "%" << " "<< b <<" " << "=" <<" "<< a%b << endl;
+    return 0;
+}
