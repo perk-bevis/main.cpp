a3a3b867bfa12f3a0a45edd7ae965ba18daf01c3
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2025-02-24 17:57:44 +0700

    Create Đếm và liệt kê ước số.cpp

---
+++ Coder.husc/Lesson 8/Đếm và liệt kê ước số.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int count = 0;
+    for(int i =1;i<=n;i++){
+      if(n%i ==0){
+        count ++;
+      }
+    }
+    cout << count << endl;
+    for(int i= 1;i<=n;i++){
+      if(n%i == 0){
+        cout << i << " ";
+      }
+    }
+    return 0;
+}
+
+//cách 2:
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int count = 0;
+    for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            count++;
+            if (i != n / i) {
+                count++;
+            }
+        }
+    }
+    cout << count << endl;
+    for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            cout << i << " ";
+        }
+    }
+
+    for (int i = sqrt(n); i >= 1; i--) {
+        if (n % i == 0 && i != n / i) {
+            cout << n / i << " ";
+        }
+    }
+
+    cout << endl;
+    return 0;
+}
