337c19afd72889a8ab33168920b0731b550e239d
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2025-02-24 18:05:57 +0700

    Update Đếm và liệt kê ước số.cpp

--- Coder.husc/Lesson 8/Đếm và liệt kê ước số.cpp
+++ Coder.husc/Lesson 8/Đếm và liệt kê ước số.cpp
@@ -53,3 +53,40 @@ int main() {
     cout << endl;
     return 0;
 }
+// cách 2 sd hàm
+#include <iostream>
+#include <cmath>
+using namespace std;
+int demuoc(int n){
+   int count = 0;
+   for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            count++;
+            if (i != n / i) {
+                count++;
+            }
+        }
+   }
+   return count;
+}
+void inuoc(int n){
+  for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            cout << i << " ";
+        }
+    }
+
+    for (int i = sqrt(n); i >= 1; i--) {
+        if (n % i == 0 && i != n / i) {
+            cout << n / i << " ";
+        }
+    }
+    cout << endl;
+}
+int main() {
+    int n;
+    cin >> n;
+    cout << demuoc(n)<< endl;
+    inuoc(n);
+    return 0;
+}
