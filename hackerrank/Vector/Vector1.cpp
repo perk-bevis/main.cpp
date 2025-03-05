4d566f9efef72f6522a955e763e6df3e3020608f
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2025-03-06 04:56:46 +0700

    Create Vector1.cpp

---
+++ hackerrank/Vector/Vector1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n);  
+    for (int i = 0; i < n; i++) {
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+    for(int i=0;i<v.size();i++){
+      cout << v[i] << " ";
+    }
+    return 0;
+}
