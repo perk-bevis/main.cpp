4130b34ab6dc60df34ee1872f994cd69eca92a59
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2025-03-06 22:02:16 +0700

    Update Giá trị lớn nhất.cpp

--- marisaoj/Lesson 3/Giá trị lớn nhất.cpp
+++ marisaoj/Lesson 3/Giá trị lớn nhất.cpp
@@ -9,16 +9,16 @@ int main()
     for(int i =0;i<n;i++){
       cin >> a[i];
     }
-    int max = a[0];
-    int vitrimax =0;
+    int max = a[0];// gán ptư đầu tiên cho mảng a
+    int vitrimax =0;// vị trí của max
     for(int i =0;i<n;i++){
-      if(max < a[i]){
+      if(max < a[i]){//max nhỏ hơn ptu hiện tại
         max = a[i];
       }
     }
     for(int i =0;i<n;i++){
       if(max == a[i]){
-        vitrimax = i;
+        vitrimax = i;//lưu vtri ptu đó vào vị trí max
         break;
       }
     }
