83d28f6f098d0630ec2b2617759536914991173f
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2025-03-06 22:15:03 +0700

    Update Giá trị lớn nhất.cpp

--- marisaoj/Lesson 3/Giá trị lớn nhất.cpp
+++ marisaoj/Lesson 3/Giá trị lớn nhất.cpp
@@ -25,3 +25,17 @@ int main()
     cout << max << " " <<vitrimax +1;
     return 0;
 }
+/*
+phân tích code: n =4
+chạy vòng lặp đầu tiên nhập giá trị 
+a[0] = 3 ;a[1] = 2 ;a[2]=2; a[3] = 3;
+gián giá trị: max = a[0] <=> max = 3
+vitrimax =0;
+chạy for:
+i=0 ;0<4 <=> max <a[0] <=> 3 <3 (sai)
+i= 1....
+i= 2...
+i=3 ;...
+i=4 dừng vòng lặp => 1 giá trị max
+
+*/
