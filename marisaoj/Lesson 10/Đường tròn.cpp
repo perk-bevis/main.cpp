7c0d37109668fa7de48cff99687d5cc9a89d3624
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2025-03-07 22:57:40 +0700

    Create Đường tròn.cpp

---
+++ marisaoj/Lesson 10/Đường tròn.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#define Max 1000
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+    int a[Max];
+    
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    
+    int x, y;
+    cin >> x >> y;
+    
+    if (x == y) {
+        cout << "0" << endl;
+        return 0;
+    }
+ 
+    x = x - 1;
+    y = y - 1;
+ 
+    int perimeter = 0;
+    for (int i = 0; i < n; i++) {
+        perimeter += a[i];
+    }
+ 
+    int fr = 0;
+    int i = x;
+    while (i != y) {
+        fr += a[i];
+        i = (i + 1) % n; 
+    }
+ 
+    int fl = perimeter - fr;
+    cout << min(fr, fl) << endl;
+ 
+    return 0;
+}
