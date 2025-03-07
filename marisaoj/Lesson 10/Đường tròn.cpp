e7f9853c4e341e50b03e1c54d9eee5d9e72311ff
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2025-03-07 23:24:11 +0700

    Update Đường tròn.cpp

--- marisaoj/Lesson 10/Đường tròn.cpp
+++ marisaoj/Lesson 10/Đường tròn.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #define Max 1000
 using namespace std;
- 
+
 int main() {
     int n;
     cin >> n;
@@ -18,24 +18,24 @@ int main() {
         cout << "0" << endl;
         return 0;
     }
- 
+    //chỉ số trong mảng a
     x = x - 1;
     y = y - 1;
- 
+    // tính chu vi
     int perimeter = 0;
     for (int i = 0; i < n; i++) {
         perimeter += a[i];
     }
- 
+    // tính khoảng cách theo chiều xuôi 
     int fr = 0;
     int i = x;
     while (i != y) {
         fr += a[i];
         i = (i + 1) % n; 
     }
- 
+    // tính khoảng cách theo chiều ngược
     int fl = perimeter - fr;
     cout << min(fr, fl) << endl;
- 
+
     return 0;
 }
