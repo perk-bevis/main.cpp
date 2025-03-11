b5e81e35f4ff2759f102d8f66da8c41ecad59963
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2025-03-11 11:35:06 +0700

    Create Dãy nón.cpp

---
+++ marisaoj/Lesson 19/Dãy nón.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+ 
+bool check(int *a, int n) {
+    if (n == 1) {
+        return true;
+    }
+    int i = 1;
+    while (i < n && a[i] > a[i - 1]) {
+        i++;
+    }
+    if (i == n) {
+        return true;
+    }
+    while (i < n && a[i] < a[i - 1]) {
+        i++;
+    }
+    return i == n;
+}
+ 
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cout << (check(a, n) ? "YES" : "NO") << endl;
+    return 0;
+}
