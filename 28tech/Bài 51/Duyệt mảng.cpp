dc7282a5ab189dadb8399e0a645b9124f7b5d452
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2025-03-11 18:30:19 +0700

    Create Duyệt mảng.cpp

---
+++ 28tech/Bài 51/Duyệt mảng.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cout <<a[i] <<" ";
+    }
+    cout << endl;
+     for (int i = n-1; i >=0; i--) {
+        cout <<a[i] <<" ";
+    }
+    cout << endl;
+    for(int i = 0; i<n;i+=2){
+      cout << a[i] << " ";
+    }
+    cout << endl;
+    for(int i = 1; i<n;i+=2){
+      cout << a[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < n - 1; i++) {
+        cout << a[i] + a[i + 1] << " ";
+    }
+    cout << endl;
+    return 0;
+}
