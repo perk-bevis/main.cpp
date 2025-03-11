50495f039b42d086c76e4a95b13a76d756cfe682
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2025-03-11 18:27:35 +0700

    Create Nhỏ hơn.cpp

---
+++ 28tech/Bài 50/Nhỏ hơn.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N; cin >> N;
+  int A[N];
+  for (int i = 0; i < N; ++i) {
+    cin >> A[i];
+  }
+  for (int i = 1; i < N - 1; ++i) {
+    if (A[i] < A[i - 1] && A[i] < A[i + 1]) {
+      cout << A[i] << " ";
+    }
+  }
+  return 0;
+}
