05e262411bce86b4b3015f0f47da8f4dc7390b50
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2025-03-12 16:37:45 +0700

    Update Ma trận xoắn ốc.cpp

--- marisaoj/Lesson 27/Ma trận xoắn ốc.cpp
+++ marisaoj/Lesson 27/Ma trận xoắn ốc.cpp
@@ -24,12 +24,14 @@ int main() {
 }
  
 void topRight(int a[][Max_cot], int x1, int y1, int x2, int y2) {
+    //trường hợp chỉ còn 1 cột
     if (y2 == y1) {
         for (int i = x1; i <= x2; i++) {
             a[i][y1] = d++;
         }
         return;
     }
+    // trường hợp chỉ còn 1 hàng
     if (x2 == x1) {
         for (int j = y1; j <= y2; j++) {
             a[x1][j] = d++;
@@ -37,14 +39,15 @@ void topRight(int a[][Max_cot], int x1, int y1, int x2, int y2) {
         return;
     }
  
-    
+    // điền hàng trên cùng từ trái qua phải
     for (int j = y1; j <= y2; j++) {
         a[x1][j] = d++;
     }
-    
+    // điền cột bên phải từ trên xuống dưới
     for (int i = x1 + 1; i <= x2; i++) {
         a[i][y2] = d++;
     }
+    //chuyển sang phần tử còn lại theo hướng bottomLeft
     if (x2 - x1 > 0 && y2 - y1 > 0) {
         x1++; y2--;
         bottomLeft(a, x1, y1, x2, y2);
@@ -52,12 +55,15 @@ void topRight(int a[][Max_cot], int x1, int y1, int x2, int y2) {
 }
  
 void bottomLeft(int a[][Max_cot], int x1, int y1, int x2, int y2) {
+    //điền hàng dưới cùng từ phải sang trái
     for (int j = y2; j >= y1; j--) {
         a[x2][j] = d++;
     }
+    // điền cột bên trái từ dưới lên trên
     for (int i = x2 - 1; i >= x1; i--) {
         a[i][y1] = d++;
     }
+    //chuyển sang phần tử còn lại theo hướng topRight
     if (x2 - x1 > 0 && y2 - y1 > 0) {
         x2--; y1++;
         topRight(a, x1, y1, x2, y2);
