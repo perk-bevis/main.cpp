c83746fab9048994e2d894897efc1a79a0288d34
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2025-05-04 21:10:30 +0700

    Create Subarray Divisibility.cpp

---
+++ USACO/PrefixSum/Lesson 4/Subarray Divisibility.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+long long subarraySum(int nums[], int size) {
+    map<long long, int> prefix_sum_freq;
+    prefix_sum_freq[0] = 1;
+
+    long long current_sum = 0;
+    long long count = 0;
+
+    for (int i = 0; i < size; ++i) {
+        current_sum += nums[i];
+        count += prefix_sum_freq[current_sum % size];
+        prefix_sum_freq[current_sum]++;
+    }
+
+    return count;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int size;
+    cin >> size;
+
+    int* nums = new int[size];
+    for (int i = 0; i < size; ++i) {
+        cin >> nums[i];
+    }
+
+    cout << subarraySum(nums, size) << "\n";
+
+    delete[] nums;
+    return 0;
+}
