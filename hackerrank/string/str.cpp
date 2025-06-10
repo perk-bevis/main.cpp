41e0b6aa79e5a39e4c1de4eb64d1205920f9f4d3
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2025-06-10 22:01:07 +0700

    Create str.cpp

---
+++ hackerrank/string/str.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+    cout << a.size() << " " << b.size() << endl;
+    cout << a + b << endl;
+    swap(a[0], b[0]);
+    cout << a << " " << b << endl;
+    return 0;
+}
+https://www.hackerrank.com/challenges/c-tutorial-strings/problem?isFullScreen=true
