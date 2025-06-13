18ab7dd9891e834f69328f103d29695c87f14323
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2025-06-13 10:43:55 +0700

    Create đọc xâu bỏ qua dòng trống.cpp

---
+++ Hoccongnghe/STRING/lesson5/đọc xâu bỏ qua dòng trống.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        if (!line.empty()) {
+            cout << line << endl;
+        }
+    }
+    return 0;
+}
