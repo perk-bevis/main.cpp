c0257710f1d8abd6335b69fbc87c67740223d029
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2025-06-13 11:29:05 +0700

    Create Chuyển số thành xâu (xâu ký tự cơ bản).cpp

---
+++ Hoccongnghe/STRING/lesson7/Chuyển số thành xâu (xâu ký tự cơ bản).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    long long s;
+    cin >> s;
+    string r = to_string(s);
+    cout << r << endl;
+    return 0;
+}
